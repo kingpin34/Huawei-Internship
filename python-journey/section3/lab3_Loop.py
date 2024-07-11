2cb940d25ee6b16bc89987120be1d6098cdd596b
kingpin34 <mkofi055@gmail.com>
2024-07-11 17:30:38 +0100

    Create lab3_Loop.py

---
+++ python-journey/section3/lab3_Loop.py
@@ -0,0 +1,28 @@
+# odd_number=0
+# even_number=0
+# number=int(input("Enter an odd number or even number:"))
+# while number != 0:
+#     if number% 2 ==1:
+#         odd_number +=1
+#     else:
+#         even_number +=1
+#     number=int(input("Enter another odd number or even number or press 0 to stop:"))
+# print("The number of odd numbers are:" ,odd_number) 
+# print("The number of even numbers are:",even_number)
+       
+print(
+    """
+      +============================+
+      | Welcome to my game ,Muggle |
+      | Enter an integer number    | 
+      |                            | 
+      | Guess what I've picked for |
+      |  you                       |
+      |  so,what is the secret     |
+      | number?                    |
+      +============================+
+    """)
+
+    
+
+ 
