cf2d0ecd505c418f715dc219d68beef34fcced65
kingpin34 <mkofi055@gmail.com>
2024-07-11 09:29:59 +0100

    simple input and output

---
+++ python-journey/section2/lab2_simple_input_output_command.py
@@ -0,0 +1,20 @@
+# input a float value for variable a here
+A=float(input("Please enter a value for a:"))
+# input a float value for variable b here
+B=float(input("please enter a value for b:"))
+#expression for sum ,subtraction and division of the two values
+C=(A + B)
+S=(A - B)
+M=(A * B)
+D=(A / B)
+# output the result of addition here
+print("the sum of the value of a and b:",C )
+# output the result of subtraction here
+print("The subtraction result is:", S)
+# output the result of multiplication here
+print("The result of multiplication of the two values is:",M)
+# output the result of division here
+print("The result of division is:" ,D)
+
+print("\nThat's all, folks!")
+
