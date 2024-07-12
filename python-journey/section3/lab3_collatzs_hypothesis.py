a6b84fda3b4a2608b383c31d6cdb8c264144ef44
kingpin34 <mkofi055@gmail.com>
2024-07-12 15:52:08 +0100

    Update lab3_collatzs_hypothesis.py

--- python-journey/section3/lab3_collatzs_hypothesis.py
+++ python-journey/section3/lab3_collatzs_hypothesis.py
@@ -1,7 +1,26 @@
-blocks=int(input("Kindly enter the number of blocks:"))
-height=0
-blocks_used=0
-while blocks_used + height +1<= blocks:
-    height+=1
-    blocks_used+= height
-print("the height of your pyramid is:",height)    
+# blocks=int(input("Kindly enter the number of blocks:"))
+# height=0
+# blocks_used=0
+# while blocks_used + height +1<= blocks:
+#     height+=1
+#     blocks_used+= height
+# print("the height of your pyramid is:",height)    
+print()
+c0=int(input("Enter a non-negative or non-zero number"))
+steps=0
+if c0 > 0:
+    while c0!=1:
+        print(c0)
+        c0=c0//2
+    else:
+        c0=3 * c0 + 1
+        steps+=1
+    print(c0)    
+    print("steps=" ,steps)
+else:    
+    print("please enter a non-zero or non-negative number:")        
+        
+  
+
+ 
+
