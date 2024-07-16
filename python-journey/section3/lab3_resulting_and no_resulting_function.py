37b8e4f91972364bff11781708e5189c25500d38
kingpin34 <mkofi055@gmail.com>
2024-07-16 16:04:23 +0100

    Update lab3_resulting_and no_resulting_function.py

--- python-journey/section3/lab3_resulting_and no_resulting_function.py
+++ python-journey/section3/lab3_resulting_and no_resulting_function.py
@@ -4,4 +4,26 @@ def boring_function():
 x = boring_function()
  
 print("The boring_function has returned its result. It's:", x)
- 
+def fox():
+    print("We will pull")
+    return 123
+
+b=fox()
+print("your code number is :",b)
+def happy_new_year( firstname,lastname):
+    print(firstname,lastname)
+happy_new_year(lastname="mensah",firstname="kofi")  
+
+def strange_list_fun(n):
+    strange_list=[]
+    for i in range(0,n):
+        strange_list.insert(0,i)
+    return strange_list
+
+print(strange_list_fun(5))
+
+
+    
+
+
+
