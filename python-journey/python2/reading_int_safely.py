9d176add047cb9d8cb152f4a515be27258871d11
kingpin34 <mkofi055@gmail.com>
2024-08-12 10:08:36 +0100

    Update reading_int_safely.py

--- python-journey/python2/reading_int_safely.py
+++ python-journey/python2/reading_int_safely.py
@@ -1,21 +1,21 @@
-# def read_int(prompt, min, max):
-#     try:
-#         value=int(input(prompt))
-#         if value < min or value > max:
-#             print("please the value you entered is not within the permitted range")
-#             return None
-#     except ValueError:
-#         print("wrong input,please input the value again")  
-#         # return None  
+# # def read_int(prompt, min, max):
+# #     try:
+# #         value=int(input(prompt))
+# #         if value < min or value > max:
+# #             print("please the value you entered is not within the permitted range")
+# #             return None
+# #     except ValueError:
+# #         print("wrong input,please input the value again")  
+# #         # return None  
     
-#     return value
+# #     return value
     
     
     
-# v = read_int("Enter a number from -10 to 10: ", -10, 10)
+# # v = read_int("Enter a number from -10 to 10: ", -10, 10)
 
-# print("The number is:", v)
-#Reads the interger value and be prom
+# # print("The number is:", v)
+# #Reads the interger value and be prom
 
 
 def read_int(prompt, min,max):
@@ -27,10 +27,10 @@ def read_int(prompt, min,max):
             print("Error:Wrong input, please enter a valid input data type")
         except:
             print("oh sorry, something happened")
-        if value < min or value >max:
-            print("Error: the value you entered is not within the permitted range of {}...{}" .format(min,max))
-        else:
-                break
+            if value < min or value >max:
+                print("Error: the value you entered is not within the permitted range of {}...{}" .format(min,max))
+            else:
+                    break
     return value
 
 
